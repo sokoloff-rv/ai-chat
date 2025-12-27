eebd4201e1a14abf503ab741fe20cf71cc2a24c4
Roman Sokolov <sokoloff.rv@gmail.com>
2025-12-27 16:30:04 +0600

    Добавлена проверка на то, что тесты не используют основную БД

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,19 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    public static function setUpBeforeClass(): void
+    {
+        parent::setUpBeforeClass();
+        $db = $_ENV['DB_DATABASE'] ?? $_SERVER['DB_DATABASE'] ?? null;
+
+        if ($db === null && function_exists('env')) {
+            $db = env('DB_DATABASE');
+        }
+
+        if ($db === 'ai_chat') {
+            throw new \RuntimeException(
+                "Tests aborted: configured to use production database."
+            );
+        }
+    }
 }
